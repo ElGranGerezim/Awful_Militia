1f6a4d4f5ec419e2f4f9d08709482c3f2001aa46
Nazanal Laughlin <Nazanal@live.com>
2022-10-31 15:12:23 -0600

    Setup Question class, questions randomly generate, buttons contain actual answers

--- app/src/main/java/com/example/mathattack/FirstFragment.kt
+++ app/src/main/java/com/example/mathattack/FirstFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.mathattack.databinding.FragmentFirstBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -19,6 +20,8 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var question : Question = AdditionQuestion(arrayOf(0,0));
+    private var difficulty = 9;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +30,6 @@ class FirstFragment : Fragment() {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,6 +50,27 @@ class FirstFragment : Fragment() {
             Snackbar.make(view, R.string.mid_button_text, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        binding.refreshButton.setOnClickListener{
+            refreshQuestion()
+        }
+
+        refreshQuestion()
+    }
+
+    fun refreshQuestion(){
+        var numbs = arrayOf<Int>()
+        numbs += Random.nextInt(difficulty + 1);
+        numbs += Random.nextInt(difficulty + 1)
+        question = when(Random.nextBoolean()){
+            true-> AdditionQuestion(numbs)
+            false-> SubtractionQuestion(numbs);
+        }
+        binding.textviewFirst.text = question.GetText()
+        val answers = question.GetAnswers()
+        binding.rightButton.text = answers[0].toString()
+        binding.midButton.text = answers[1].toString()
+        binding.leftButton.text = answers[2].toString()
     }
 
     override fun onDestroyView() {
